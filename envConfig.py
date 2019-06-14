372a730558426c43ccd788a053a1fa8e9de3417e
Victor_Lenovo <vict7422@gmail.com>
2019-06-14 08:19:37 -0500

    Se modificarion rutas utilizando os(Dinamismo)

--- envConfig.py
+++ envConfig.py
@@ -1,3 +1,6 @@
-VAGRANTSERVICEHOME = "/Users/john/.vagrantservice"
-VAGRANTPROJECT = "/home/cherno/vagran/"
-JSONFILE="/home/cherno/Servidores/Flask-Vagrant-RC_cluster/DB.json"
+import os
+
+HOME = os.environ["HOME"]
+VAGRANTSERVICEHOME = os.environ["HOME"] + "/.vagrantservice"
+VAGRANTPROJECT = os.environ["HOME"] + "/vagrant/"
+JSONFILE = os.environ["HOME"] + "/.vagrantservice/DB.json"

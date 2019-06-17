1ec5f08c75a3dc37d48877e3827e4c77534c8fc3
Victor_Lenovo <vict7422@gmail.com>
2019-06-17 13:46:33 -0500

    test.py meth VagrantUP listo

--- test.py
+++ test.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
 import subprocess
-import time
 import threading
 
-stop_output = False
 
-def f(p):
-    global stop_output
+def VagrantUP():
+    command = subprocess.Popen(["vagrant","up"],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     while True:
-        l = p.stdout.readline()
-        if not l or stop_output:
+        lines = command.stdout.readline()
+        if not lines:
             break
-        print(l.rstrip())   # or whatever you want to do with the line
+        print(lines.rstrip())
 
-airodump = subprocess.Popen(["vagrant","up"],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-#t = threading.Thread(target=f,args=(airodump,))
-#t.start()
-#time.sleep(145)
-# kill the process and stop the display after 10 seconds whatever happens
-#airodump.terminate()
-#stop_output = True
+
+th = threading.Thread(target=VagrantUP)
+th.start()
+th.join()
 #https://living-sun.com/es/python/734175-python-subprocess-cannot-read-output-of-airodump-nd-mon0-python-subprocess-aircrack-ng.html

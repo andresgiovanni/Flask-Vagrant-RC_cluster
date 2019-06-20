c0f734bd554ce6a2e4b50c8bf70b66e0faeee9e8
VictorMCL-Notebook <vict7422@gmail.com>
2019-06-20 09:38:24 -0500

    Se cambia ruta de proyectos

--- envConfig.py
+++ envConfig.py
@@ -2,5 +2,5 @@ import os
 
 HOME = os.environ["HOME"]
 VAGRANTSERVICEHOME = os.environ["HOME"] + "/.vagrantservice"
-VAGRANTPROJECT = os.environ["HOME"] + "/vagrant/"
+VAGRANTPROJECT = os.environ["HOME"] + "/.vagrantservice/proyects/"
 JSONFILE = os.environ["HOME"] + "/.vagrantservice/DB.json"
